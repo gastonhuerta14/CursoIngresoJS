8f08160269a18a5a394f3100a34c4205a528aaca
gastonhuerta14 <40763501+gastonhuerta14@users.noreply.github.com>
2018-07-02 12:07:03 -0300

    ejercicios 1 2 3

---
+++ jsEntradaSalida-3.js
@@ -0,0 +1,18 @@
+/*Debemos lograr tomar un dato por 'ID'
+y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
+function mostrar()
+{
+	
+	var nombre;
+	nombre=document.getElementById('elNombre').value;
+	alert("Su nombre");
+	
+	/*
+		
+		alert("ejercicio 3");
+		console.log("nombre");
+
+	*/
+}
+
+
