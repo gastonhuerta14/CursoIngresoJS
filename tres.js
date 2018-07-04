7da58309103b5a814dd456df179c64fdef498071
gastonhuerta14 <40763501+gastonhuerta14@users.noreply.github.com>
2018-07-04 12:19:12 -0300

    Add files via upload

---
+++ tres.js
@@ -0,0 +1,16 @@
+function mostrar()
+{
+	var precio
+	var porcentaje
+	var descuento
+	var precioydescuento
+	var IVA
+	precio=prompt("ingrese el precio");
+	porcentaje=("ingrese porcentaje");
+	precioydescuento=parseInt("100")*parseInt(precio)/(parseInt("100")-parseInt(descuento));
+	descuento=parseInt(descuento)*parseInt(precio)/parseInt("100");
+	IVA=parseInt("79")*parseInt()
+	alert("el descuento en dinero es de "+descuento+" el precio con el descuento es "+);
+	
+
+}
