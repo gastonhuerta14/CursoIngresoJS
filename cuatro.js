efbb618834f0c00babe046d4b559978109cc5461
gastonhuerta14 <40763501+gastonhuerta14@users.noreply.github.com>
2018-07-12 00:17:58 -0300

    Add files via upload

--- cuatro.js
+++ cuatro.js
@@ -6,9 +6,18 @@ function mostrar()
 	var resta;
 	numero1=prompt("ingrese numero uno aqui");
 	numero2=prompt("ingrese numero dos aqui");
-	suma=paseInt(numero1)+parseInt(numero2);
-	resta=parseInt(numero1)-parseInt(numero2);
-	if (numero1==numero2) {alert(numero1+"  "+numero2+"  =  "+suma);} 
-	else if (numero1>numero2) {alert(numero1+"  "+numero2+"  =  "+resta);} 
-	else if (suma>10) {alert("la suma es : "+suma+" y supero el 10");}
+	suma=parseInt(numero1)/parseInt(numero2);
+	division=parseInt(numero1)-parseInt(numero2);
+	if (numero1==numero2) 
+		{
+			alert(numero1+" + "+numero2+"  =  "+suma);
+		} 
+	if (numero1>numero2) 
+		{
+			alert(numero1+" / "+numero2+"  =  "+division);
+		} 
+	if (suma<50) 
+		{
+			alert("la suma es : "+suma+" y supero el 10");
+		}
 }
---
+++ jsInstruccionSwitch(1).js
@@ -0,0 +1,20 @@
+function mostrar()
+{
+//tomo la edad  
+var mesDelAño = document.getElementById('mes').value;
+switch(mesDelAño){
+
+	case"Enero": alert("que comience bien el año");
+	break;
+	case"Marzo": alert("a clases");
+	break;
+	case"Julio": alert("se vienen las vacaciones");
+	break;
+	case"Diciembre": alert("felices fiestas");
+	break;
+
+}
+
+
+
+}//FIN DE LA FUNCIÓN
